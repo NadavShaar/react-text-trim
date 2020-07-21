1d605683565a0d9cf48c24a9a285c4dfc42b5d64
Nadav Shaar <nadavshaar@gmail.com>
2020-07-22 01:28:00 +0300

    Updates

---
+++ precache-manifest.2bce704b5830ec8fe9298c4af2b3648a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ef8336729a98be5f16a4e528778605",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a19b0ce17ee83ed6d221",
+    "url": "./static/css/main.cff1283d.chunk.css"
+  },
+  {
+    "revision": "207226b1e27200760d7c",
+    "url": "./static/js/2.a72d8d7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a72d8d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19b0ce17ee83ed6d221",
+    "url": "./static/js/main.d75b010d.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.d75b010d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774af7ac034ebd3d35c4",
+    "url": "./static/js/runtime-main.964bdb2c.js"
+  }
+]);
